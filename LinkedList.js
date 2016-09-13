38a155e70d40d08a664e18190bcc778ffa7cf6b9
Ryan Collins <RyanCCollins@users.noreply.github.com>
2016-09-13 09:26:24 -0400

    Chore: upload LinkedList JS representation

---
+++ LinkedList.js
@@ -0,0 +1,128 @@
+class LinkedList {
+  constructor() {
+    this.textIndex = this.testIndex.bind(this);
+    this.tail = null;
+    this.head = null;
+    this.length = 0;
+  }
+  push(value) {
+    const node = new Node(value);
+    this.length ++;
+    if (!this.head) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  }
+  pop() {
+     return this.delete(this.length - 1);
+  }
+  get(index) {
+    const node = this.find(index, this.testIndex);
+    return node.value || null;
+  }
+  find(value, test=this.test) {
+    let current = this.head;
+    let i = 0;
+    while (current) {
+      if (test(value, current.value, i)) {
+        return current;
+      }
+      current = current.next;
+      i++;
+    }
+    return null;
+  }
+  delete(index) {
+    if (index === 0) {
+      const head = this.head
+      if (head) {
+        this.head = head.next;
+      } else {
+        this.head = null;
+        this.tail = null;
+      }
+      this.length--;
+      return head.value;
+    }
+    const node = this.find(index - 1, this.testIndex);
+    const excise = node.next;
+    if (!excise) return null;
+    node.next = excise.next;
+    if (node.next && !node.next.next) {
+      this.tail = node.next;
+    }
+    this.length--;
+    return excise.value;
+  }
+  test(a, b) {
+    return a === b;
+  }
+  testIndex(search, __, i) {
+    return search === i;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+
+// unit tests
+// do not modify the below code
+describe('LinkedList', function() {
+  const range = length => Array.apply(null, {length: length}).map(Number.call, Number);
+  const abcRange = length => range(length).map( num => String.fromCharCode( 97 + num ) );
+  let list;
+  
+  beforeEach( () => {
+    list = new LinkedList();
+  })
+  
+  it('constructor', () => {
+    expect(list).toEqual(jasmine.any(LinkedList));
+  });
+  
+  it('push', () => {
+    abcRange(26).map( character => list.push(character) );
+    expect(list.length).toEqual(26);
+  });
+  
+  it('pop', () => {
+    abcRange(13).map( character => list.push(character) );
+    expect(list.length).toEqual(13);
+    range(10).map( () => list.pop() );
+    expect(list.length).toEqual(3);
+    expect(list.pop()).toEqual('c');
+  });
+  
+  it('get', () => {
+    list.push('first');
+    expect(list.get(0)).toEqual('first');
+    list.push('second');
+    expect(list.get(1)).toEqual('second');
+    expect(list.get(0)).toEqual('first');
+    abcRange(26).map( character => list.push(character));
+    expect(list.get(27)).toEqual('z');
+    expect(list.get(0)).toEqual('first');
+    expect(list.get(9)).toEqual('h');
+    list.pop();
+    expect(list.get(list.length-1)).toEqual('y');
+  });
+  
+  it('delete', () => {
+    abcRange(26).map( character => list.push(character) );
+    list.delete(13);
+    expect(list.length).toEqual(25);
+    expect(list.get(12)).toEqual('m');
+    expect(list.get(13)).toEqual('o');
+    list.delete(0);
+    expect(list.length).toEqual(24);
+    expect(list.get(0)).toEqual('b');
+  });
+  
+});
